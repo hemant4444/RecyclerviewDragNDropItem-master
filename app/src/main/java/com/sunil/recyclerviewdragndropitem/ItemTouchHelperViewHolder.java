6901fe3e7be6bca26f7f1b65939a0f1033079f99
Hemant Moudgil <MPROGA1\hemant.moudgil@a1-d-d03.mproga1.com>
2017-12-29 01:30:55 +0530

    Initial commit

---
+++ app/src/main/java/com/sunil/recyclerviewdragndropitem/ItemTouchHelperViewHolder.java
@@ -0,0 +1,10 @@
+package com.sunil.recyclerviewdragndropitem;
+
+import android.support.v7.widget.helper.ItemTouchHelper;
+
+public interface ItemTouchHelperViewHolder {
+
+    void onItemSelected();
+
+    void onItemClear();
+}
